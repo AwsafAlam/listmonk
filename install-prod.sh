6ca77385ff59453df380c24d4b989531d8b4924a
Md Awsaf Alam <1505114.maaa@ugrad.cse.buet.ac.bd>
2023-08-02 11:38:59 +0600

    Added prod config

--- install-prod.sh
+++ install-prod.sh
@@ -91,39 +91,39 @@ get_containers() {
 }
 
 modify_config(){
-	info "generating a random password"
-	db_password=$(generate_password)
+	# info "generating a random password"
+	# db_password=$(generate_password)
 
 	info "modifying config.toml"
 	# Replace `db.host=localhost` with `db.host=db` in config file.
-	sed -i "s/host = \"localhost\"/host = \"listmonk_db\"/g" config.toml
+	# sed -i "s/host = \"localhost\"/host = \"listmonk_db\"/g" config.toml
 	# Replace `db.password=listmonk` with `db.password={{db_password}}` in config file.
 	# Note that `password` is wrapped with `\b`. This ensures that `admin_password` doesn't match this pattern instead.
-	sed -i "s/\bpassword\b = \"listmonk\"/password = \"$db_password\"/g" config.toml
+	# sed -i "s/\bpassword\b = \"listmonk\"/password = \"$db_password\"/g" config.toml
 	# Replace `app.address=localhost:9000` with `app.address=0.0.0.0:9000` in config file.
 	sed -i "s/address = \"localhost:9000\"/address = \"0.0.0.0:9000\"/g" config.toml
 
 	info "modifying docker-compose.yml"
-	sed -i "s/POSTGRES_PASSWORD=listmonk/POSTGRES_PASSWORD=$db_password/g" docker-compose.yml
+	# sed -i "s/POSTGRES_PASSWORD=listmonk/POSTGRES_PASSWORD=$db_password/g" docker-compose.yml
 }
 
 run_migrations(){
 	info "running migrations"
-	docker-compose up -d db
-	while ! is_healthy listmonk_db; do sleep 3; done
+	# docker-compose up -d db
+	# while ! is_healthy listmonk_db; do sleep 3; done
 	docker-compose run --rm app ./listmonk --install
 }
 
 start_services(){
 	info "starting app"
-	docker-compose up -d app db
+	docker-compose up -d app
 }
 
 show_output(){
 	info "finishing setup"
 	sleep 3
 
-	if is_running listmonk_db && is_running listmonk_app
+	if is_running listmonk_app
 	then completed "Listmonk is now up and running. Visit http://localhost:9000 in your browser."
 	else
 		error "error running containers. something went wrong."
@@ -132,9 +132,9 @@ show_output(){
 
 
 check_dependencies
-check_existing_db_volume
-get_config
-get_containers
+# check_existing_db_volume
+# get_config
+# get_containers
 modify_config
 run_migrations
 start_services
